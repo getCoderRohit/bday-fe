eb316e4c2af63e7559ffe35665bfe48a679c8c00
getCoderRohit <rohithegde444@gmail.com>
2021-09-21 20:01:00 +0530

    first build

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
 	return (
 		<div className="App">
 			<header className="App-header">
-				<p>Hello World</p>
+				<p>Happy Birthday Bhondu</p>
 			</header>
 		</div>
 	);
